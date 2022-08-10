55ad0787ccf64de5933aa9c7723abfa4285d9671
Tanmay <tanmay.jain@particle41.com>
2022-08-10 10:18:04 +0530

    Basic app working with homepage component

---
+++ components/Homepage.tsx/index.tsx
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <div>
+      <div className="p-3 text-center bg-light">
+        <h1 className="mb-1">Welcome to Pokedox</h1>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
